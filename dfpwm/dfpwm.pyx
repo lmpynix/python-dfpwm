68872c9722da48187e17517d530e38b0cfcffc25
cyan <contact@cyans.me>
2023-12-26 18:48:49 +0800

    feat: resample from ndarray chore: bump version

--- dfpwm/dfpwm.pyx
+++ dfpwm/dfpwm.pyx
@@ -137,6 +137,12 @@ cpdef compressor(
     free(<void*>array)
     return result
 
-def resample(io: "BinaryIO") -> tuple["np.ndarray", float]:
+def resample_from_file(io: "BinaryIO") -> tuple["np.ndarray", float]:
+    import warnings
     import librosa
+    warnings.warn("resample_from_file is deprecated, use dfpwm.resample instead")
     return librosa.load(io, dtype='float32', sr=SAMPLE_RATE)
+
+def resample(data: np.ndarray, origin_samplerate: float, target_sample_rate = SAMPLE_RATE):
+    import librosa
+    return librosa.resample(data, origin_samplerate, target_sample_rate)
